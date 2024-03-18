1ff56a5fabdb99a349c1158b9d2e0253c78dc911
mrunal chaudhari 666 <mrunalchaudhari171@gmail.com>
2024-03-18 16:37:42 +0530

    commited

---
+++ quiz app/quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -0,0 +1,28 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.Question;
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("questions")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("AllQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+    @GetMapping("Category/{cat}")
+    public ResponseEntity<List<Question>> getQuestionBYCategory(@PathVariable("cat") String Category){
+        return questionService.getQuestionsByCategory(Category);
+    }
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+}
---
+++ quiz app/quizapp/src/main/java/com/example/quizapp/service/QuestionService.java
@@ -0,0 +1,48 @@
+package com.example.quizapp.service;
+
+import com.example.quizapp.Question;
+import com.example.quizapp.dao.Questiondao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+@Autowired
+    Questiondao questiondao;
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questiondao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questiondao.findByCategory(category),HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questiondao.save(question);
+            return new ResponseEntity<>("success",HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new String(),HttpStatus.BAD_REQUEST);
+
+
+    }
+}
